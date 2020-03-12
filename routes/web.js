526b4a4b6e73eedd31a1b7f4fe85590fc9cf2ad2
Lien Vo <nlvo@live.nl>
2020-03-12 23:57:42 +0100

    add character detail page

--- routes/web.js
+++ routes/web.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express.Router();
 const characters = require('../controllers/characters')
+const character = require('../controllers/character')
 const search = require('../controllers/search')
 const comics = require('../controllers/comics')
 const comic = require('../controllers/comic')
 const browse = require('../controllers/browse')
 
 app
-    // .get('/', characters.showAllCharacters)
+    .get('/', characters.showAllCharacters)
     .get('/browse', browse)
     .get('/comics', comics.showAllComics)
     .get('/creators', characters.showAllCharacters)
     .get('/characters', characters.showAllCharacters)
+    .get('/characters/:id', character)
     .get('/series', characters.showAllCharacters)
     .get('/events', characters.showAllCharacters)
     .get('/search', search.showSearchResults)
