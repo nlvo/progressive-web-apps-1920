c934feaea6307827f34494a471ebb371e764df65
Lien Vo <nlvo@live.nl>
2020-03-16 20:50:22 +0100

    edit views structure and add layouts folder

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const web = require('./routes/web');
+
+const app = express();
+const port = process.env.PORT;
+
+app
+	.engine('hbs', exphbs({
+		extname: 'hbs',
+		layoutsDir: 'views',
+		partialsDir: 'views',
+	}))
+	.set('view engine', 'hbs')
+	.use(express.static('public'))
+	.use(express.json())
+	.use(web)
+	.listen(port);
