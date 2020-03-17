fafbb2ec6195ff24ff91cd38919d7da008e0fc27
Lien Vo <nlvo@live.nl>
2020-03-17 17:26:16 +0100

    add caching to service worker and edit app icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,35 @@
 self.addEventListener('install', event => {
 	console.log('install');
-	self.skipWaiting();
+	event.waitUntil(
+		caches
+		.open('v1')
+		.then(cache => {
+				return cache
+					.addAll([
+						'/',
+						'/img/favicon.png',
+						'/css/styles.css',
+						'/js/main.js',
+					])
+					.then(() => self.skipWaiting());
+		})
+	);
 });
 
 self.addEventListener('activate', event => {
 	console.log('activating')
 	event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(response => {
+				if(response) {
+					return response
+				}
+				return fetch(event.request)
+			})
+	)
 });
