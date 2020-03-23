5c66c0a66f134bda129bc66e839ebfdbdf51740c
Lien Vo <nlvo@live.nl>
2020-03-23 18:46:59 +0100

    remove custom icon font

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,6 @@ self.addEventListener('install', event => {
 						'/img/marvel-logo.svg',
 						'/favicon.png',
 						'/css/style.min.css',
-						'/fonts/icomoon.ttf',
 						'/js/main.min.js',
 					])
 					.then(() => self.skipWaiting());
