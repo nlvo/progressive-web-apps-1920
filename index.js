ba676d56ebcbaf6c842710a77a4bbb73d84aa86d
Lien Vo <nlvo@live.nl>
2020-03-23 21:22:28 +0100

    fix failed to lookup view

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
 const web = require('./routes/web');
 
 const app = express();
@@ -13,6 +14,7 @@ app
 		partialsDir: 'views',
 	}))
 	.set('view engine', 'hbs')
+	.set('views', path.join(__dirname, 'views/'))
 	.use(express.static('public'))
 	.use(express.json())
 	.use(web)
