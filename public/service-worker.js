14ae439e5e20af6ed0c278f8a752cb9322de4cff
Lien Vo <nlvo@live.nl>
2020-03-24 23:58:59 +0100

    edit sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,17 @@
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches
-		.open('v1')
-		.then(async (cache) => {
-				await cache.addAll([
-				'/offline',
-				'/img/marvel-logo.svg',
-				'/img/avengers-logo.svg',
-				'/css/style.min.css',
-			]);
-			return self.skipWaiting();
-		})
+			.open('v1')
+			.then(async (cache) => {
+				await cache
+					.addAll([
+						'/offline',
+						'/img/marvel-logo.svg',
+						'/img/avengers-logo.svg',
+						'/css/style.min.css',
+					]);
+				return self.skipWaiting();
+			}),
 	);
 });
 
@@ -22,11 +23,11 @@ self.addEventListener('fetch', event => {
 	event.respondWith(
 		caches
 			.match(event.request)
-			.then(response => {
-				if(response) {
-					return response
+			.then((response) => {
+				if (response) {
+					return response;
 				}
-				return fetch(event.request)
-			})
-	)
+				return fetch(event.request);
+			}),
+	);
 });
