993c1023ee02bddfb4b414b50959220e68d1894b
Lien Vo <nlvo@live.nl>
2020-03-26 23:23:54 +0100

    fix show more comics on click

--- controllers/comics.js
+++ controllers/comics.js
@@ -2,40 +2,31 @@ const fetch = require('./data');
 const endpoint = require('./endpoint.js');
 
 // create new endpoint for all comics
-function createEightEndpoint(pages) {
+function createEightEndpoint () {
 	const offset = 0;
 	const limit = 8;
-	const total = pages * limit;
-	const comicsEndpoint = endpoint('comics', `dateDescriptor=thisMonth&orderBy=-onsaleDate&limit=${total}&offset=${offset}`);
+	const comicsEndpoint = endpoint('comics', `dateDescriptor=thisMonth&orderBy=-onsaleDate&limit=${limit}&offset=${offset}`);
 	return comicsEndpoint;
 }
 
 // get all comics and return when available
-async function getEight(pages) {
-	const comicsEndpoint = createEightEndpoint(pages);
+async function getEight () {
+	const comicsEndpoint = createEightEndpoint();
 	const comics = await fetch(comicsEndpoint);
 	return comics;
 }
 
-let pages = 1;
-
 // create new endpoint for all comics
-function createComicsEndpoint() {
-	pages++;
-	const maxComics = 80;
+function createComicsEndpoint (pageNumber) {
 	const comics = 8;
-	const amountComics = pages * comics;
-	const startFrom = amountComics - comics;
-	const pageLimit = amountComics / pages;
-	if (amountComics < maxComics) {
-		const comicsEndpoint = endpoint('comics', `dateDescriptor=thisMonth&orderBy=-onsaleDate&limit=${pageLimit}&offset=${startFrom}`);
-		return comicsEndpoint;
-	}
+	const totalComics = pageNumber * comics;
+	const comicsEndpoint = endpoint('comics', `dateDescriptor=thisMonth&orderBy=-onsaleDate&limit=${comics}&offset=${totalComics}`);
+	return comicsEndpoint;
 }
 
 // get all comics and return when available
-async function getMore(req, res) {
-	const comicsEndpoint = createComicsEndpoint();
+async function getMore (req, res) {
+	const comicsEndpoint = createComicsEndpoint(req.query.id);
 	const comics = await fetch(comicsEndpoint);
 	res.render('more', {
 		layout: false,
@@ -44,9 +35,8 @@ async function getMore(req, res) {
 }
 
 // render comics when data is available
-async function showAll(req, res) {
-	const page = req.query.id || 1;
-	const comics = await getEight(page);
+async function showAll (req, res) {
+	const comics = await getEight();
 	res.render('main', {
 		comics,
 		comicsPageTitle: 'All comics',
@@ -58,6 +48,3 @@ module.exports = {
 	getMore,
 	getEight,
 };
-// const pages = req.query.page || 1;
-// console.log(req.query.page)
-// const comicsEndpoint = endpoint('comics', `dateDescriptor=thisMonth&orderBy=-onsaleDate&offset=${offset}&limit=${pages * limit}`);
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,21 @@
 const moreComics = document.querySelector('.more');
 const section = document.querySelector('.comics');
+let pages = 0;
 
 function next () {
-
+	const maxComics = 80;
+	const comics = 8;
+	const totalComics = pages * comics;
+	if (totalComics < maxComics) {
+		pages += 1;
+	}
+	return pages;
 }
 
 async function showComics () {
 	const base = location.pathname;
-	const data = await fetch(`${base}/page`);
+	const currentPage = next();
+	const data = await fetch(`${base}/page?id=${currentPage}`);
 	const n = await data.text();
 	section.insertAdjacentHTML('beforeend', n);
 }
