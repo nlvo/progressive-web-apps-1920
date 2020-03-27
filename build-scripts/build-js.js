708868f8bdfc22ddb25d9a93c625edb64f77f6e8
Lien Vo <nlvo@live.nl>
2020-03-27 22:03:28 +0100

    edit minify javascript and exclude source

--- build-scripts/build-js.js
+++ build-scripts/build-js.js
@@ -1,10 +1,8 @@
 const gulp = require('gulp');
-const rename = require('gulp-rename');
 const minify = require('gulp-minify');
 
 gulp.src([
 	'./src/js/main.js',
 ])
-	.pipe(minify())
-	.pipe(rename('main.min.js'))
+	.pipe(minify({ noSource: true }))
 	.pipe(gulp.dest('./public/js'));
